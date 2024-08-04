4fecae0fe2a3e20a7f3f7264be594b9e9bcf8013
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-04 14:39:44 +0330

    feat(card): make the static card for book

--- Phase06-Angular/src/app/app.component.ts
+++ Phase06-Angular/src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
+import { CardComponent } from "./components/card/card.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HeaderComponent, CardComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'Phase06-Angular';
+  title = "Phase06-Angular";
 }
