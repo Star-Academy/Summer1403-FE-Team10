bb65f1bf74f7fd4b746728a7aa35702b504995f1
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-14 09:59:23 +0330

    feat(style): modal add book

--- Phase06-Angular/src/app/app.component.ts
+++ Phase06-Angular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BookListComponent } from './components/book-list/book-list.component';
+import { NewBookComponent } from './components/new-book/new-book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, BookListComponent],
+  imports: [RouterOutlet, HeaderComponent, BookListComponent, NewBookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
