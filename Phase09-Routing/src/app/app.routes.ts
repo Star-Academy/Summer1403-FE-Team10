13f649e3e98bba02f3769c1eec0165c1cf243e34
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-16 23:39:28 +0330

    feat(practice2): practice 2 done

--- Phase09-Routing/src/app/app.routes.ts
+++ Phase09-Routing/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContactComponentComponent } from './components/contact-component/contact-component.component';
 import { AboutComponentComponent } from './components/about-component/about-component.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   {
-    path: 'conatct',
+    path: 'contact',
     component: ContactComponentComponent,
   },
   {
