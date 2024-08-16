b89b86a5256b9d96b9d386135a509c8a9795284c
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-16 19:53:12 +0330

    feat(practice2): practice 2 done

--- Phase07-RxJS/src/app/app.routes.ts
+++ Phase07-RxJS/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { Practice1Component } from './components/practice1/practice1.component';
+import { Practice2Component } from './components/practice2/practice2.component';
+import { Practice3Component } from './components/practice3/practice3.component';
+import { Practice4Component } from './components/practice4/practice4.component';
 
 export const routes: Routes = [
   {
@@ -8,14 +11,14 @@ export const routes: Routes = [
   },
   {
     path: 'practice2',
-    component: Practice1Component,
+    component: Practice2Component,
   },
   {
     path: 'practice3',
-    component: Practice1Component,
+    component: Practice3Component,
   },
   {
     path: 'practice4',
-    component: Practice1Component,
+    component: Practice4Component,
   },
 ];
