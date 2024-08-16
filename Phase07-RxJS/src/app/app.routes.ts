32aa5d12f6a585d8b7d70805ad9a5f14dc75eb9e
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-16 19:41:14 +0330

    feat(cleanup): cleanup router and markup

--- Phase07-RxJS/src/app/app.routes.ts
+++ Phase07-RxJS/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Practice1Component } from './components/practice1/practice1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'practice1',
+    component: Practice1Component,
+  },
+  {
+    path: 'practice2',
+    component: Practice1Component,
+  },
+  {
+    path: 'practice3',
+    component: Practice1Component,
+  },
+  {
+    path: 'practice4',
+    component: Practice1Component,
+  },
+];
